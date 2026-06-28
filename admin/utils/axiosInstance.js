234466e530b51b57d555710f973e0d58b20afb11
hulkode <alhassanahmedsoliman@gmail.com>
2026-06-28 19:03:47 +0300

    spo

--- admin/utils/axiosInstance.js
+++ admin/utils/axiosInstance.js
@@ -17,6 +17,11 @@ axiosInstance.interceptors.response.use(
       if (error.response.status === 401) {
         // Redirect to login page
         window.location.href = "/login";
+      }
+
+      if (error.response.status === 403) {
+        // Redirect to login page
+        window.location.href = "/register";
       } else if (error.response.status === 500) {
         console.error("Server error. Please try again later.");
       }
